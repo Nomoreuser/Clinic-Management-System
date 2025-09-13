d51a343242122c5f6f7b2b13e02f58d3d9baa782
Nomoreuser <>
2025-09-13 13:52:49 +0800

    try sa phone

---
+++ StudentSystem/student.js
@@ -0,0 +1,12 @@
+
+
+function Verify(){
+  if(studentID.value == ""){
+    alert("Please enter your Student ID")
+  }
+  if(studentID.value.length < 11){
+    alert("11 chars")
+  }if(studentID.value.length = 11){
+    window.location.href="main.html";
+  }
+}
